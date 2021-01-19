5508030f83d3cda23b88f141b9ec639c44ff38d2
huidongchen <hd7chen@gmail.com>
2021-01-19 18:52:50 -0500

    add new module `integration`

--- simba/tools/__init__.py
+++ simba/tools/__init__.py
@@ -5,3 +5,4 @@ from ._general import (
 )
 from ._umap import umap
 from ._gene_scores import gene_scores
+from ._integration import anchors
---
+++ simba/tools/_integration.py
@@ -0,0 +1,68 @@
+"""Integration across experimental conditions or single cell modalities"""
+
+import numpy as np
+import anndata as ad
+from sklearn.utils.extmath import randomized_svd
+from sklearn.metrics.pairwise import pairwise_distances
+from scipy.sparse import csr_matrix
+
+
+def anchors(adata_ref,
+            adata_query,
+            feature='highly_variable',
+            n_components=20,
+            random_state=42,
+            n_top_anchors=6000,
+            percentile=0.01,
+            layer=None,
+            metric='euclidean',
+            **kwargs,
+            ):
+    """Identify anchors across experimental conditions or single cell modalities
+    Parameters
+    ----------
+    adata: AnnData
+        Annotated data matrix.
+
+    Returns
+    -------
+    updates `adata` with the following fields:
+    """
+    mask_ref = adata_ref.var[feature]
+    feature_ref = adata_ref.var_names[mask_ref]
+    feature_query = adata_query.var_names
+    feature_shared = list(set(feature_ref).intersection(set(feature_query)))
+    if layer is None:
+        X_ref = adata_ref[:, feature_shared].X
+        X_query = adata_query[:, feature_shared].X
+    else:
+        X_ref = adata_ref[:, feature_shared].layers[layer]
+        X_query = adata_query[:, feature_shared].layers[layer]
+    mat = X_ref * X_query.T
+
+    U, Sigma, VT = randomized_svd(mat,
+                                  n_components=n_components,
+                                  random_state=random_state,
+                                  **kwargs)
+    cca_data = np.vstack((U, VT.T))
+    X_cca_ref = cca_data[:U.shape[0], :]
+    X_cca_query = cca_data[-VT.shape[1]:, :]
+    X_cca_ref = X_cca_ref / (X_cca_ref**2).sum(-1, keepdims=True)**0.5
+    X_cca_query = X_cca_query / (X_cca_query**2).sum(-1, keepdims=True)**0.5
+
+    dist_ref_query = pairwise_distances(X_cca_ref,
+                                        X_cca_query,
+                                        metric=metric)
+    if n_top_anchors is None:
+        cutoff = np.percentile(dist_ref_query.flatten(), percentile)
+    else:
+        cutoff = np.partition(dist_ref_query.flatten(),
+                              n_top_anchors-1)[n_top_anchors-1]
+    id_x, id_y = np.where(dist_ref_query <= cutoff)
+    conn_ref_query = csr_matrix((dist_ref_query[id_x, id_y],
+                                 (id_x, id_y)),
+                                shape=dist_ref_query.shape)
+    adata_ref_query = ad.AnnData(X=conn_ref_query,
+                                 obs=adata_ref.obs,
+                                 var=adata_query.obs)
+    return adata_ref_query

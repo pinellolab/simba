d6ac8f938fc237b373c8994e0de6166b5b56a8f0
huidongchen <hd7chen@gmail.com>
2021-09-29 21:15:32 -0400

    put back simba pkg

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,8 +12,8 @@
 #
 import os
 import sys
-# sys.path.insert(0, os.path.abspath('../simba'))
-# import simba
+sys.path.insert(0, os.path.abspath('../simba'))
+import simba
 
 
 # -- Project information -----------------------------------------------------
@@ -23,8 +23,7 @@ copyright = '2021, Huidong Chen'
 author = 'Huidong Chen'
 
 # The full version, including alpha/beta/rc tags
-# release = simba.__version__
-release = 1.0
+release = simba.__version__
 
 
 # -- Retrieve notebooks (borrowed from scVelo) -------------------------------

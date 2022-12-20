44f4e2702bc465946fefbbcc8d379e3f2f012c15
Jayoung Ryu <jayoung_ryu@g.harvard.edu>
2022-12-19 22:14:07 -0500

    editing test script

--- tests/test_pbg_training.py
+++ tests/test_pbg_training.py
@@ -31,18 +31,17 @@ def test_pbg_training_rna_significance(adata_CG, tmp_path):
                     get_marker_significance=True,
                     dirname='graph0')
     si.tl.pbg_train(auto_wd=True,
-                    output='model')
+                    output='model',
+                    get_marker_significance=True)
     si.pl.pbg_metrics(fig_ncol=1,
                       save_fig=True)
-    dict_adata = si.read_embedding()
+    dict_adata = si.read_embedding(get_marker_significance=True)
     adata_C = dict_adata['C'] 
     adata_G = dict_adata['G'] 
     adata_nG = dict_adata['nG']
     adata_cmp = si.tl.compare_entities(adata_ref=adata_C, adata_query=adata_G, adata_query_null=adata_nG)
     assert "max_fdr" in adata_cmp.var.columns
     assert ((adata_cmp.var["max_fdr"] <= 1.0) & (adata_cmp.var["max_fdr"] >= 0.0)).all()
-    si.pl.entity_metrics(adata_cmp,
-        x='max', y='gini', cutoff_fdr=0.05, color_by_fdr='gini', save_fig=True, fig_path='.')
 
 def test_pbg_training_atac(adata_CP, tmp_path):
     si.settings.set_workdir(tmp_path / "simba_atac")
